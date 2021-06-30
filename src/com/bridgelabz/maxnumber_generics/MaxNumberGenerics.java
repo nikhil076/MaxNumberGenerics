0ec1c62c6e31ac268eff419f83870f20a9fe3c01
nikhil076 <nikhil.arvind7@gmail.com>
2021-06-30 15:43:28 +0530

    Added the functionality to find maximum of 3 float variaables

--- src/com/bridgelabz/maxnumber_generics/MaxNumberGenerics.java
+++ src/com/bridgelabz/maxnumber_generics/MaxNumberGenerics.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class MaxNumberGenerics {
 
-	public static void toPrint(Integer integerVal1,Integer integerVal2,Integer integerVal3, Integer maxInteger)
+	public static void toPrintInteger(Integer integerVal1,Integer integerVal2,Integer integerVal3, Integer maxInteger)
 	{
 		maxInteger=integerVal1;
 		if(integerVal2.compareTo(maxInteger)>0)
@@ -15,19 +15,28 @@ public class MaxNumberGenerics {
 		{
 			maxInteger=integerVal3;
 		}
-		System.out.println("the Max integer number is :"+maxInteger);
+		System.out.println("the Max Integer number is :"+maxInteger);
+	}
+	
+	public static void toPrintFloat(float floatVal1,float floatVal2,float floatVal3, float maxFloat)
+	{
+		maxFloat=floatVal1;
+		if(Float.compare(floatVal2, maxFloat)>0)
+		{
+			maxFloat=floatVal2;
+		}
+		if(Float.compare(floatVal3, maxFloat)>0)
+		{
+			maxFloat=floatVal3;
+		}
+		System.out.println("the Max Float number is :"+maxFloat);
 	}
 
 	public static void main(String[] args) {
 		Integer maxInteger= 0;
+		Integer maxFloat= 0;
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Enter First integer value :");
-		int integerVal1 = scanner.nextInt();
-		System.out.println("Enter Second integer value :");
-		int integerVal2 = scanner.nextInt();
-		System.out.println("Enter Third integer value :");
-		int integerVal3 = scanner.nextInt();
-		
-		toPrint(integerVal1, integerVal2, integerVal3, maxInteger);
+		toPrintInteger(10, 50, 20, maxInteger);
+		toPrintFloat(10.2f, 102.23f, 10.45f, maxInteger);
 	}
 }
