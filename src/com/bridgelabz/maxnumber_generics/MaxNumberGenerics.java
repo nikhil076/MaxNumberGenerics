86e9c9728b3fc5455970249956eeca7602072f5d
nikhil076 <nikhil.arvind7@gmail.com>
2021-06-30 14:57:27 +0530

    Added the functionality to find maximum of 3 integers

--- src/com/bridgelabz/maxnumber_generics/MaxNumberGenerics.java
+++ src/com/bridgelabz/maxnumber_generics/MaxNumberGenerics.java
@@ -1,8 +1,33 @@
 package com.bridgelabz.maxnumber_generics;
 
+import java.util.Scanner;
+
 public class MaxNumberGenerics {
 
+	public static void toPrint(Integer integerVal1,Integer integerVal2,Integer integerVal3, Integer maxInteger)
+	{
+		maxInteger=integerVal1;
+		if(integerVal2.compareTo(maxInteger)>0)
+		{
+			maxInteger=integerVal2;
+		}
+		if(integerVal3.compareTo(maxInteger)>0)
+		{
+			maxInteger=integerVal3;
+		}
+		System.out.println("the Max integer number is :"+maxInteger);
+	}
+
 	public static void main(String[] args) {
-		System.out.println("Welcome to max number problem");
+		Integer maxInteger= 0;
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter First integer value :");
+		int integerVal1 = scanner.nextInt();
+		System.out.println("Enter Second integer value :");
+		int integerVal2 = scanner.nextInt();
+		System.out.println("Enter Third integer value :");
+		int integerVal3 = scanner.nextInt();
+		
+		toPrint(integerVal1, integerVal2, integerVal3, maxInteger);
 	}
 }
