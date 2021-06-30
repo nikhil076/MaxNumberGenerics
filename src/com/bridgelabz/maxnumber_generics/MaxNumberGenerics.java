455caa7eeef62581e147eaa30548af02eece4c60
nikhil076 <nikhil.arvind7@gmail.com>
2021-06-30 16:14:51 +0530

    Added the method to find maximum of 3 string values

--- src/com/bridgelabz/maxnumber_generics/MaxNumberGenerics.java
+++ src/com/bridgelabz/maxnumber_generics/MaxNumberGenerics.java
@@ -1,9 +1,7 @@
 package com.bridgelabz.maxnumber_generics;
 
-import java.util.Scanner;
-
-public class MaxNumberGenerics {
-
+public class MaxNumberGenerics 
+{
 	public static void toPrintInteger(Integer integerVal1,Integer integerVal2,Integer integerVal3, Integer maxInteger)
 	{
 		maxInteger=integerVal1;
@@ -18,7 +16,7 @@ public class MaxNumberGenerics {
 		System.out.println("the Max Integer number is :"+maxInteger);
 	}
 	
-	public static void toPrintFloat(float floatVal1,float floatVal2,float floatVal3, float maxFloat)
+	public static void toPrintFloat(Float floatVal1,Float floatVal2,Float floatVal3, Float maxFloat)
 	{
 		maxFloat=floatVal1;
 		if(Float.compare(floatVal2, maxFloat)>0)
@@ -31,12 +29,26 @@ public class MaxNumberGenerics {
 		}
 		System.out.println("the Max Float number is :"+maxFloat);
 	}
+	
+	public static void toPrintString(String stringVal1,String stringVal2,String stringVal3, String maxString)
+	{
+		maxString=stringVal1;
+		if(stringVal2.compareTo(maxString)>0)
+		{
+			maxString=stringVal2;
+		}
+		if(stringVal3.compareTo(maxString)>0)
+		{
+			maxString=stringVal3;
+		}
+		System.out.println("the Max String number is :"+maxString);
+	}
 
 	public static void main(String[] args) {
 		Integer maxInteger= 0;
-		Integer maxFloat= 0;
-		Scanner scanner = new Scanner(System.in);
+		Float maxFloat= 0f;
 		toPrintInteger(10, 50, 20, maxInteger);
-		toPrintFloat(10.2f, 102.23f, 10.45f, maxInteger);
+		toPrintFloat(10.2f, 102.23f, 10.45f, maxFloat);
+		toPrintString("Apple", "banana", "Pear", null);
 	}
 }
