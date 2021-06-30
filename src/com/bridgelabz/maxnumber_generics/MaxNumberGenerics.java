43754989d96022b04471aebea053520a39c631d4
nikhil076 <nikhil.arvind7@gmail.com>
2021-06-30 16:57:47 +0530

    Refactored all the 3 method in one generic method to find maximum

--- src/com/bridgelabz/maxnumber_generics/MaxNumberGenerics.java
+++ src/com/bridgelabz/maxnumber_generics/MaxNumberGenerics.java
@@ -2,9 +2,9 @@ package com.bridgelabz.maxnumber_generics;
 
 public class MaxNumberGenerics 
 {
-	public static void toPrintInteger(Integer integerVal1,Integer integerVal2,Integer integerVal3, Integer maxInteger)
+	public static <E extends Comparable<E>> void toPrint(E integerVal1,E integerVal2,E integerVal3)
 	{
-		maxInteger=integerVal1;
+		E maxInteger=integerVal1;
 		if(integerVal2.compareTo(maxInteger)>0)
 		{
 			maxInteger=integerVal2;
@@ -15,40 +15,10 @@ public class MaxNumberGenerics
 		}
 		System.out.println("the Max Integer number is :"+maxInteger);
 	}
-	
-	public static void toPrintFloat(Float floatVal1,Float floatVal2,Float floatVal3, Float maxFloat)
-	{
-		maxFloat=floatVal1;
-		if(Float.compare(floatVal2, maxFloat)>0)
-		{
-			maxFloat=floatVal2;
-		}
-		if(Float.compare(floatVal3, maxFloat)>0)
-		{
-			maxFloat=floatVal3;
-		}
-		System.out.println("the Max Float number is :"+maxFloat);
-	}
-	
-	public static void toPrintString(String stringVal1,String stringVal2,String stringVal3, String maxString)
-	{
-		maxString=stringVal1;
-		if(stringVal2.compareTo(maxString)>0)
-		{
-			maxString=stringVal2;
-		}
-		if(stringVal3.compareTo(maxString)>0)
-		{
-			maxString=stringVal3;
-		}
-		System.out.println("the Max String number is :"+maxString);
-	}
 
 	public static void main(String[] args) {
-		Integer maxInteger= 0;
-		Float maxFloat= 0f;
-		toPrintInteger(10, 50, 20, maxInteger);
-		toPrintFloat(10.2f, 102.23f, 10.45f, maxFloat);
-		toPrintString("Apple", "banana", "Pear", null);
+		toPrint(10, 50, 20);
+		toPrint(10.2f, 102.23f, 10.45f);
+		toPrint("Apple", "banana", "Pear");
 	}
 }
