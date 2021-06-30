d883236b545b3bb8b1086db4ecbef5d8df26eb60
nikhil076 <nikhil.arvind7@gmail.com>
2021-06-30 14:55:15 +0530

    Initialized the program

---
+++ src/com/bridgelabz/maxnumber_generics/MaxNumberGenerics.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.maxnumber_generics;
+
+public class MaxNumberGenerics {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to max number problem");
+	}
+}
